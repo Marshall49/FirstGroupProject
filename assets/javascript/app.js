83af457f39668e95d4e434cf18a91eb800ce09f4
Luke Chambers <lukechambers91@gmail.com>
2017-06-15 18:17:42 -0400

    hosintg test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,16 @@
 $.ajax({
         url: "https://developer.nps.gov/api/v0/alerts?parkCode=yell",
-        method: "GET",
         headers: {"Authorization": "A0AF4455-7229-468C-942B-5BE75278BCE5"},
-        crossDomain: true,
+        method: "GET",
+        // crossDomain: true,
+        dataType: "json",
+        // jsonp: 'jsonp',
+        xhrFields: {
+        	withCredentials: true
+        },
+        success: function() { alert("Success"); },
+    	error: function() { alert('Failed!'); },
       }).done(function(response) {
         console.log(response);
     });
+
