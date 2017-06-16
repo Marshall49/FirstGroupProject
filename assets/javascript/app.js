db3f56e7557eb3e05f8170ce8c7f91d889dba75d
Luke Chambers <lukechambers91@gmail.com>
2017-06-15 21:08:25 -0400

    initial xml to json

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,49 @@
 $.ajax({
-        url: "https://developer.nps.gov/api/v0/alerts?parkCode=yell",
-        headers: {"Authorization": "A0AF4455-7229-468C-942B-5BE75278BCE5"},
+        url: "http://api.amp.active.com/camping/campgrounds?pstate=CO&amenity=4001&sewer=3007&api_key=tevg8efq2e83yy8hzhhp99ns",
         method: "GET",
-        // crossDomain: true,
-        dataType: "json",
-        // jsonp: 'jsonp',
-        xhrFields: {
-        	withCredentials: true
-        },
-        success: function() { alert("Success"); },
-    	error: function() { alert('Failed!'); },
       }).done(function(response) {
         console.log(response);
+        console.log(xmlToJson(response));
     });
 
+function xmlToJson(xml) {
+
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	// If just one text node inside
+	if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
+		obj = xml.childNodes[0].nodeValue;
+	}
+	else if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+}
