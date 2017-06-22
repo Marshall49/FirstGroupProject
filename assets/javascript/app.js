b89eb90ba5bf42d0cf9ecf4da9216b0af9297706
Malcolm Strong <strong.malcolm@yahoo.com>
2017-06-21 21:31:47 -0400

    added some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,20 @@
+// On form submittal, using submit to avoid button per design scheme
 $("#addressForm").submit( function(event) {
     event.preventDefault();
-    address = $("#addressInput").val();
+    // Sets address to user input
+    var address = $("#addressInput").val().trim();
+    // Passes address to build google api query
     googleUrl(address);
+    // Reset form
     $("form").trigger("reset");
   });
 
 $.ajax({
-        url: "http://localhost:3000/states/wi",
-        method: "GET",
-        success: function(data){
-        console.log(data)}
-      })
+	url: "http://localhost:3000/states/wi",
+	method: "GET",
+	success: function(data){
+		console.log(data);
+		var campSites = data;
+	}
+})
+
