12bef50af28cdf0302c88137969a91db7a40ec20
Luke Chambers <lukechambers91@gmail.com>
2017-06-30 22:43:31 -0400

    added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -56,7 +56,7 @@ function googleAPI(googleURL) {
 	      		$("#map").empty(); //clears any content from #map
 				var homeLat = response.results[0].geometry.location.lat; //go into the returned json and fetch the latitude via the given path and assign it to a varable
 				var homeLng = response.results[0].geometry.location.lng; //go into the returned json and fetch the longitude via the given path and assign it to a varable
-				homeLoc = {lat: homeLat, lng: homeLng}; //build an object with the lat and long information and assign it to the homeLoc Varable
+				homeLoc = {lat: homeLat, lng: homeLng}; //build an object with the lat and long information and assign it to the homeLoc Varable for the submitted address
 			   	stateGiver(response); //send the reponse json to the the stateGiver function
 				$("#map").css("display", "block"); //displays map id
 			    lastElementTop = $('#map').position().top ; //finds position of map id
@@ -74,17 +74,6 @@ function googleAPI(googleURL) {
   });
 }
 
-function findCountry(response) {
-	for (j = 0; j < response.results[0].address_components.length; j++) {
-	  for (h = 0; h < response.results[0].address_components[j].types.length; h++) {
-	    if (response.results[0].address_components[j].types[h] === "country") {
-	      country = response.results[0].address_components[j].short_name;
-	  	};
-	  };
-	};
-	return country;
-};
-
 //This function takes in a json object retuned from a GoogleMaps API call and finds the two letter state code
 //we have to use a double for loop to search through the json for the state code it searches through each address
 //componant (first loop) then searches through each address componant type (second loop) and find the address
@@ -100,6 +89,18 @@ function stateGiver(response) {
 	};
 	// geoList(state);
 	geoJson(state);
+};
+
+//This function tests which country the submitted address originates. This function works just like stateGiver
+function findCountry(response) {
+	for (j = 0; j < response.results[0].address_components.length; j++) {
+	  for (h = 0; h < response.results[0].address_components[j].types.length; h++) {
+	    if (response.results[0].address_components[j].types[h] === "country") {
+	      country = response.results[0].address_components[j].short_name;
+	  	};
+	  };
+	};
+	return country;
 };
 
 //This function queries the server and askes it to return a json object of all the campground geocoordinates
@@ -123,79 +124,82 @@ function geoList(state) {
 }
 
 //This function works exactly like the geoList function except this function has a slightly different URL
-//specifically geojason vs geocode. There server knows how to hadle this difference
+//specifically geojason vs geocode. There server knows how to hadle this difference. This function also 
+//takes the json returned from the server and parses it and fills up some global array variables which 
+//are later used to fill in the information about the campsites when clicked on the map
 function geoJson(state) {
 	$.ajax( {
         url: "https://campsites123.herokuapp.com/geojson/" + state,
         method: "GET",
         success: function(response){
-        for (i = 0; i < response.length; i++) {
-        	var lat = Number(response[i].lat);
-        	var lng = Number(response[i].long);
-        	campSites.push({'lat': lat, 'lng': lng});
-        	campName.push(response[i].facilityName);
-        	petsAllowed.push(convertYesNo(response[i].petsAllowed));
-        	sewerHookup.push(convertYesNo(response[i].sewerHookup));
-        	waterHookup.push(convertYesNo(response[i].waterHookup));
-        	waterFront.push(convertYesNo(response[i].waterFront));
-        	facilityPhoto.push(response[i].facilityPhoto)
+        for (i = 0; i < response.length; i++) { //looks through the response json and pulls out information
+        	var lat = Number(response[i].lat); //pulls out lat from json and assigns to variable
+        	var lng = Number(response[i].long); //pulls out long from json and assigns to variable
+        	campSites.push({'lat': lat, 'lng': lng}); //puts lat and long in an array of objects called campSites
+        	campName.push(response[i].facilityName); //fills the campName array
+        	petsAllowed.push(convertYesNo(response[i].petsAllowed)); //fills the petAllowed array uses convertYesNo
+        	sewerHookup.push(convertYesNo(response[i].sewerHookup)); //fills the sewerHookup array uses convertYesNo
+        	waterHookup.push(convertYesNo(response[i].waterHookup)); //fills the waterHookup array uses convertYesNo
+        	waterFront.push(convertYesNo(response[i].waterFront)); //fills the waterFront array uses convertYesNo
+        	facilityPhoto.push(response[i].facilityPhoto); //fills the faciliy photo array with the photo url
     	}
-    	initMap();
+    	initMap(); //calls initMap function
       }
     })
 }
 
-function convertYesNo(str) {
-	if(str === "Y") {
-		return "Yes"
-	} else if (str === "N") {
-		return "No"
-	} else {
-		return "Unknown"
+//The amity informations returned from the server confirms with a "Y", "N", or "" this function converts
+//those the "Yes", "No", or "Unknown"
+function convertYesNo(str) { //function takes in a string
+	if(str === "Y") { //does that string equal "Y"
+		return "Yes" //then return the string "Yes"
+	} else if (str === "N") { //does that string equal "N"
+		return "No" //then return the string "No"
+	} else { //does that string equal something besides "Y" or "N"
+		return "Unknown" //then return the string Unknown
 	}
 }
 
+//This functions takes in string of all lowercase letters and returns that string in titlecase 
+//(First letter of each word capitalized)
 function toTitleCase(str) {
-    return str.replace(/(?:^|\s)\w/g, function(match) {
-        return match.toUpperCase();
+    return str.replace(/(?:^|\s)\w/g, function(match) { //searches a string spaces (\s) and new words (\w)
+        return match.toUpperCase(); //changes the beginning of the word to uppercase
     });
 }
 
+//This function intializes the google map in the DOM which will be filled with marker icons 
 function initMap() {
-	// sets zoom location
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 9,
-		//center: homeLoc
-		center: homeLoc,
-    	scrollwheel: false
+	var map = new google.maps.Map(document.getElementById('map'), { //looks for id in the HTML named map and sets it to display the google map
+		zoom: 9, //sets zoom for this google map
+		center: homeLoc, //centers the map on the gps coordinates for the submited address
+    	scrollwheel: false //prevents the map from sooming with the mouse scrool wheel
 	});
-	//creates a marker for the searched address
-	var homeMarker = new google.maps.Marker({
-		position: homeLoc,
-		title: "Searched Address",
-		icon: "assets/images/homeIcon.png",
-		map: map
-		});
 	
-	//  campSites[] from campAPI json object
-	for(i=0; i < campSites.length; i++){
-		var siteName = toTitleCase(he.decode(campName[i]).toLowerCase());
-		var marker = new google.maps.Marker({
-		position: campSites[i],
-		// title fix for special characters
-		title: siteName,
-		icon: "assets/images/mapmarker.png",
-		map: map
+	var homeMarker = new google.maps.Marker({ //creates a marker for the submited address
+		position: homeLoc, //places a marker on the gps coordinates saved in the homeLoc object (the submitted address)
+		title: "Searched Address", //this title presented when you hover over the icon for the submitted address
+		icon: "assets/images/homeIcon.png", //uses this image for the icon of the submitted address
+		map: map //sets the map to displayed the map generated above
+	});
+	
+	for(i=0; i < campSites.length; i++){ //this loop searches thought the arrays filled in the geojson function and fills in the information to be displayed on the map
+		var siteName = toTitleCase(he.decode(campName[i]).toLowerCase()); //takes ith element from the campName array and changes it to lowercase then uses the he.js library of function (extrally downloaded) and convert char numbers to their corresponding characters then pases it to the titlecase function and assigns it to a variable
+		var marker = new google.maps.Marker({ //makes a marker to be used for the ith element of the campSites array
+		position: campSites[i], //positions the marker in on the gps corrdinates for the ith element of the campSites array
+		title: siteName, //this title presented when you hover over the icon for the submitted address
+		icon: "assets/images/mapmarker.png", //uses this image for the icon
+		map: map //sets the map to displayed the map generated above
 		});
-		console.log(facilityPhoto)
+		//the below line will fills in the information for the infoWindow which popsup when you click on the icons in the map. it gets this information from the array filled in the geojson function
 		var content = "<div style='text-align:left;'><div style='font-size:15px;font-weight: bold;'>Name: " + siteName + "</div>" + "<img src='http://www.reserveamerica.com/"+ facilityPhoto[i] + "'>" + "<br>Pets Allowed: " + petsAllowed[i] + "<br>Sewer Hookup: " + sewerHookup[i] + "<br>Water Hookup: " + waterHookup[i] + "<br>Water Front: " + waterFront[i] + "</div>";
-		var infowindow = new google.maps.InfoWindow();
-		google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
-        return function() {
-           infowindow.setContent(content);
-           infowindow.open(map,marker);
-        };
-    })(marker,content,infowindow));
+		var infowindow = new google.maps.InfoWindow(); //created an infowindow to be displayed when you click on a icon in the man
+		google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ //listnes for the user to lick on of the isons in the map
+	        return function() { //returns the information to be presented in the infowindoe
+	           infowindow.setContent(content); //fills the content in the infowindow with the content variable set above
+	           infowindow.open(map,marker); //opens the infowindow in the map
+	        };
+	    })(marker,content,infowindow));
 	};
 }
 
